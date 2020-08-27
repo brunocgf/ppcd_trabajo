b14ae3ac374bd14930f6ed82118086ee5a1416b4
brunocgf <brunocgf@gmail.com>
2020-08-27 12:35:27 -0500

    primer commit

---
+++ 2020_08_18_Linea_de_comandos.sh
@@ -0,0 +1,26 @@
+ ##
+ > UFO-Dic-2014.tsv cut -f1,3 | tr '\t' ' ' | cut -d ' ' -f1,3 | sort -k 2 -k 1 | uniq -c | sort -n -r -k 1 | head -10
+
+ ##
+ python -m http.server 8888
+ python -m http.server 8888 &
+
+
+## utf
+iconv -f ISO-8859-15 -t utf-8 UFO-Nov-2014.tsv > ~/clean.txt
+
+## curl
+curl -I http://www.gutenberg.org
+
+## g rep
+grep -v "18:" UFO-Dic-2014.tsv | wc -l
+grep -E "18:|19:|20:" UFO-Dic-2014.tsv
+grep -o -E "[Bb]lue|[Oo]range" UFO-Dic-2014.tsv | tr -s '[:upper:]' '[:lower:]' | sort | uniq -c
+grep -E "[0-9]{2}" UFO-Dic-2014.tsv | grep "12/13" | tee ocurrio.txt | wc -l
+grep -vE "[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}" UFO-Dic-2014.tsv 
+
+## awk
+awk -F"[\t]" '/blue/ {print $3 $1}' ufo/UFO-Nov-2014.tsv 
+awk '{sum += $1} END {print "SUMA: "sum }' numbers.dat
+awk -F',' '{sum1 += $1, sum2 += $2} END {print sum1, sum2}' numbers.dat  #tiene error
+awk -F"[\t]" '/minutes/ {print $5}' UFO-Nov-2014.tsv | awk '{sum += $1/60} END {print sum " horas"}
